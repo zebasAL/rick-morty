77693d4f754d2535f3df9ed3d48e7f1d61d2d971
zebasAL <zebastian_al@hotmail.com>
2022-10-17 00:50:47 -0500

    git workflows, vercel link and .env vars, cypress and jest config, cache-provider, next-auth, error-boundary component was created

---
+++ apps/web/utils/ErrorBoundary.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode } from "react";
+import * as Sentry from "@sentry/nextjs";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };  
+  }
+
+  componentDidCatch(error: Error, errorInfo: any) {
+    // we can log error or errorInfo to Sentry here.
+    // errorInfo will contain the details 
+    // and stack trace which is better for debugging
+  
+    // log the error to an error reporting service
+    Sentry.captureException(errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // render custom fallback UI
+      return (
+        <div style={{ textAlign: "center", margin: "20px 10px" }}>
+          <img
+            style={{ maxHeight: "200px", width: "100%", objectFit: "contain" }}
+            alt="error" src="https://metiormedia.s3.us-east-1.amazonaws.com/assets/errorImage.png"
+          />
+          <h2>Woops!, An Error Occurred</h2>
+          <p> The application detected an error, and it"s been reported to the application development team. You can try refreshing the page.</p>
+          <p>If the error keeps occurring, please file a bug report with the following details, and include any steps to reproduce the issue.</p>    
+        </div>
+      );
+    };
+    return this.props.children; 
+  }
+}
