bd6cf621a204f9838b885b494510f70712ae06f9
ricardo <rm09961219@gmail.com>
2023-03-15 14:41:02 -0700

    Updated next.config.js

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { useState, useContext } from "react";
-import { ThemeContext } from "../ThemeContext"
+import { Button } from "ui";
+import { ThemeContext } from "../ThemeContext";
 
 export default function Home() {
   const { theme, toggleThemeHandler } = useContext(ThemeContext);
-  const [value, setValue] = useState<string>('')
-  console.log('theme', theme)
+  const [value, setValue] = useState<string>("");
+  console.log("theme", theme);
 
   return (
     <>
@@ -12,23 +13,27 @@ export default function Home() {
 
       <input value={value} onChange={(e) => setValue(e.target.value)} />
 
-      <div style={{ border: 'red 1px solid', width: '500px', height: '300px', margin: 'auto' }}>
+      <div
+        style={{
+          border: "red 1px solid",
+          width: "500px",
+          height: "300px",
+          margin: "auto",
+        }}
+      >
         <h3>Set Theme</h3>
         <div>
-          <button onClick={() => toggleThemeHandler('light')}>
+          <button onClick={() => toggleThemeHandler("light")}>
             Light theme
           </button>
-          <button onClick={() => toggleThemeHandler('morty')}>
+          <button onClick={() => toggleThemeHandler("morty")}>
             Morty theme
           </button>
-          <button onClick={() => toggleThemeHandler('rick')}>
-            Rick theme
-          </button>
-          <button onClick={() => toggleThemeHandler('dark')}>
-            Dark theme
-          </button>
+          <button onClick={() => toggleThemeHandler("rick")}>Rick theme</button>
+          <button onClick={() => toggleThemeHandler("dark")}>Dark theme</button>
         </div>
       </div>
+      <Button />
       <p>Texto</p>
     </>
   );
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,3 +1,18 @@
 export const Button = () => {
-  return <button>Boop</button>;
+  return (
+    <button
+      style={{
+        background: "black",
+        color: "white",
+        border: "none",
+        padding: "0.5rem 1rem",
+        borderRadius: "0.25rem",
+        display: "inline-block",
+        cursor: "pointer",
+      }}
+      onClick={() => console.log("you clicked me")}
+    >
+      My ui button
+    </button>
+  );
 };
