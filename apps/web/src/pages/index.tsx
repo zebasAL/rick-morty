0d1e95fd2402bccb72c0b15ad90aaa50e966dadc
ricardo <rm09961219@gmail.com>
2023-04-05 16:36:54 -0700

    Update accessibility and implemented getStaticProps

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,6 +1,5 @@
-import AllCharacters from "@/components/AllCharacters";
-
-export default function Home() {
+function Home(props) {
+  const { characters } = props;
   return (
     <div className="home-wrap">
       <div className="home-title">
@@ -9,8 +8,34 @@ export default function Home() {
       {/** intended to be a carrousel*/}
       <div className="home-subtitle">
         <h4 style={{ marginTop: 0 }}>Some of the characters in the show</h4>
-        <AllCharacters />
+        <div className="data-wrap">
+          {characters?.map((character) => (
+            <div key={character.id} className="data-items">
+              {character.name}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await res.json();
+
+  // By returning { props: { characters } }, the Blog component
+  // will receive `characters` as a prop at build time
+  return {
+    props: {
+      characters: data.results,
+    },
+  };
+}
+
+export default Home;
