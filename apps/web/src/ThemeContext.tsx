7642a553ed5ad0754077f6bf27b330e5b0860856
Ricardo Medina <rm09961219@gmail.com>
2023-04-10 21:52:01 -0700

    update index with getStaticProps

--- apps/web/src/ThemeContext.tsx
+++ apps/web/src/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactElement, useEffect, useState } from "react";
+import { createContext, ReactElement, useState } from "react";
 
 export const ThemeContext = createContext({
   theme: "light",
@@ -14,51 +14,42 @@ const isBrowser = typeof document !== "undefined";
 
 const isLocalStorageEmpty = (): boolean => !localStorage.getItem("data-theme");
 
+const initialThemeHandler = (): Themes => {
+  if (!isBrowser) return "light";
+  if (isLocalStorageEmpty()) {
+    const systemTheme = window.matchMedia("prefers-color-scheme: dark")?.media
+      ? "dark"
+      : "light";
+    localStorage.setItem("data-theme", systemTheme);
+    document.documentElement.setAttribute("data-theme", systemTheme);
+    return systemTheme;
+  } else {
+    const storedTheme: string = localStorage.getItem("data-theme") ?? "light";
+    if (
+      storedTheme === "dark" ||
+      storedTheme === "light" ||
+      storedTheme === "rick" ||
+      storedTheme === "morty"
+    ) {
+      document.documentElement.setAttribute("data-theme", storedTheme);
+      return storedTheme;
+    }
+    return "light";
+  }
+};
+
 export const ThemeContextProvider = (
   props: ThemePropsInterface
 ): ReactElement => {
-  const [themeLoaded, setThemeLoaded] = useState<boolean>(false);
-  const [currentTheme, setCurrentTheme] = useState<Themes>("light");
-
-  const setInitialTheme = () => {
-    if (!isBrowser) return;
-    if (isLocalStorageEmpty()) {
-      const systemTheme = window.matchMedia("prefers-color-scheme: dark")?.media
-        ? "dark"
-        : "light";
-      localStorage.setItem("data-theme", systemTheme);
-      document.documentElement.setAttribute("data-theme", systemTheme);
-      setCurrentTheme(systemTheme);
-    } else {
-      const storedTheme: string = localStorage.getItem("data-theme") ?? "light";
-      if (
-        storedTheme === "dark" ||
-        storedTheme === "light" ||
-        storedTheme === "rick" ||
-        storedTheme === "morty"
-      ) {
-        document.documentElement.setAttribute("data-theme", storedTheme);
-        setCurrentTheme(storedTheme as Themes);
-      } else {
-        setCurrentTheme("light");
-      }
-    }
-    setThemeLoaded(true);
-  };
-
-  useEffect(() => {
-    setInitialTheme();
-  }, []);
+  const [currentTheme, setCurrentTheme] = useState<Themes>(
+    initialThemeHandler()
+  );
 
   const toggleThemeHandler = (nextTheme: Themes): void => {
     setCurrentTheme(nextTheme);
     document.documentElement.setAttribute("data-theme", `${nextTheme}`);
     localStorage.setItem("data-theme", `${nextTheme}`);
   };
-
-  if (!themeLoaded) {
-    return null;
-  }
 
   return (
     <ThemeContext.Provider value={{ theme: currentTheme, toggleThemeHandler }}>
